cb0bf72dad0e5786fc5233754ef69a12250252ad
Jason Kuhrt <jasonkuhrt@me.com>
2021-08-26 21:03:50 -0400

    feat: check for presence of api key envar
    closes #5

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-console.log('todo')
+const envarSpecs = {
+  VISUAL_CROSSING_WEATHER_API_KEY: {
+    name: `VISUAL_CROSSING_WEATHER_API_KEY`,
+  },
+}
+
+if (!process.env[envarSpecs.VISUAL_CROSSING_WEATHER_API_KEY.name]) {
+  console.log(
+    `Missing environment variable "${envarSpecs.VISUAL_CROSSING_WEATHER_API_KEY.name}". You need to set this to the value of your Visual Crossig API key. If you do not have an API key yet then please get one at https://www.visualcrossing.com/weather/weather-data-services#/login.`
+  )
+  process.exit(1)
+}
